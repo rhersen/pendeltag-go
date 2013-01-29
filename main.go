94198062a5c0746aa6a746de667fc271241bf0d2
Rudolf Hersén <rhersen@gmail.com>
2013-01-29 21:59:48 +0100

    no key

--- main.go
+++ main.go
@@ -7,7 +7,8 @@ import (
 )
 
 func main() {
-	resp, err := http.Get("https://api.trafiklab.se/sl/realtid/GetDpsDepartures.json?key=8589732b19f6c9a78b004aff74f28d98&siteId=9525&timeWindow=60")
+	key := "get your own at trafiklab.se"
+	resp, err := http.Get("https://api.trafiklab.se/sl/realtid/GetDpsDepartures.json?key=" + key + "&siteId=9525&timeWindow=60")
 
 	if err != nil {
 		fmt.Println(err)
