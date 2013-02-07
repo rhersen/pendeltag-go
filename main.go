e7fcb894bc1a2fc34972bd7c95549bab31d30cb3
Rudolf Hersén <rhersen@gmail.com>
2013-02-07 17:49:11 +0100

    functions for creating the structure

--- main.go
+++ main.go
@@ -6,14 +6,28 @@ import (
 	"net/http"
 )
 
+func a0() map[string]string {
+	return map[string]string{}
+}
+
+func ec(name text, child ...writer) writer {
+	return element{name, child, a0()}
+}
+
+func ea(attributes map[string]string) writer {
+	return element{"link", []writer{}, attributes}
+}
+
 func index(w http.ResponseWriter, r *http.Request) {
 	heading := text("The Stand Alone Web App")
 	fmt.Fprintf(w, "<!DOCTYPE html>")
-	element{"head", []writer{
-			element{"title", []writer{heading}, map[string]string{}},
-			element{"link", []writer{}, map[string]string{"rel":"stylesheet","href":"/css/"}}},
-		map[string]string{}}.write(w)
-	element{"body", []writer{element{"h1", []writer{heading}, map[string]string{}}}, map[string]string{}}.write(w)
+
+	linkAttributes := map[string]string{
+		"rel":"stylesheet",
+		"href":"/css/"}
+
+	ec("head", ec("title", heading), ea(linkAttributes)).write(w)
+	ec("body", ec("h1", heading)).write(w)
 }
 
 func css(w http.ResponseWriter, r *http.Request) {
