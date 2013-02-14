9b68c154aaf3719e8d9b9a3b559d0b15ca0e2d00
Rudolf Hersén <rudolf@3517u.(none)>
2013-02-14 21:49:27 +0100

    using a port that is available on my server

--- main.go
+++ main.go
@@ -10,5 +10,5 @@ func main() {
 	http.HandleFunc("/js/", js)
 	http.HandleFunc("/station/", station)
 	http.HandleFunc("/departures/", departures)
-    http.ListenAndServe(":8080", nil)
+    http.ListenAndServe(":4000", nil)
 }
